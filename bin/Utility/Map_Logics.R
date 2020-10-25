45dcfee06d94e7b8db18dfd442546ddcbbc342c4
RRobert92 <robert.kiewisz@gmail.com>
2020-10-25 15:32:50 +0100

    BUgfix in UI

--- bin/Utility/Map_Logics.R
+++ bin/Utility/Map_Logics.R
@@ -23,21 +23,21 @@ Map_server <- function(input, output, session){
     })
     
     output$Avg_speed <- renderText(paste(Ship_statistic[1,1], "kn", sep = " "))
-    output$Full_distance <<- renderText(paste(round(as.numeric(Ship_statistic[1,2]/60,
-                                                               2)),
-                                              "H", sep = " "))
+    output$Full_distance <<- renderText(paste(round(Ship_statistic[1,2],
+                                                               2),
+                                              "km", sep = " "))
     output$Long_speed <- renderText(paste(
       round((Ship_data_selected[1,3] + Ship_data_selected[2,3])/2,
             2), 
       "kn", sep = " "))
     
     output$Long_time <- renderText(paste(
-      abs(as.numeric((Ship_data_selected[1,9] - Ship_data_selected[2,9]))), 
+      round(abs(as.numeric((Ship_data_selected[1,9] - Ship_data_selected[2,9]))),2), 
       "min", sep = " "))
     
     output$Long_distance <- renderText(paste(paste(
       round(geo_point_dist(as.matrix(Ship_data_selected[1:2, 1:2])),2),
-      "kn", sep = " ")))
+      "km", sep = " ")))
     
     output$Ship_table <- DT::renderDataTable({Ship_data_selected[c(1,2,4,5,9)]})
   })
